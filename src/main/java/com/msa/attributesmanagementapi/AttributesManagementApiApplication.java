0c0c070d5430e1f08ba292a65e43c1dda31aae59
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-03-03 13:28:48 +0200

    First Commit

--- src/main/java/com/msa/attributesmanagementapi/AttributesManagementApiApplication.java
+++ src/main/java/com/msa/attributesmanagementapi/AttributesManagementApiApplication.java
@@ -10,19 +10,6 @@ public class AttributesManagementApiApplication {
 		SpringApplication.run(AttributesManagementApiApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner runner(UserAttributesRepository repository){
-//		return args -> {
-//			UserAttributes user;
-//			TimeSchedule timeSchedule = TimeSchedule.getInstance();
-//
-//			timeSchedule.setStartTime(LocalTime.of(9, 0));
-//			timeSchedule.setEndTime(LocalTime.of(17, 0));
-//			timeSchedule.setDaysOfWeek(Set.of("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"));
-//
-//			user = new UserAttributes("204209","Engineer","Software Management",timeSchedule,5,"Level 1","Active");
-//			repository.insert(user);
-//		};
-//	}
+
 
 }
